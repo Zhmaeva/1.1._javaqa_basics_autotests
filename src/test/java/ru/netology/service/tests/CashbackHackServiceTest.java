9472c797e479bedb48e0bebe5ae6ab1876a7adbb
Ekaterina Zhmaeva <ekaterina.eduardovn@gmail.com>
2024-03-08 15:41:20 +0300

    Add jupiter

--- src/test/java/ru/netology/service/tests/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/tests/CashbackHackServiceTest.java
@@ -1,38 +1,39 @@
 package ru.netology.service.tests;
 
-import org.junit.Assert;
-import org.junit.Test;
+
 import ru.netology.service.CashbackHackService;
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.Assertions;
 
 public class CashbackHackServiceTest {
 
-    @Test
+    @org.junit.Test
     public void shouldNotGetCashbackLessBoundary() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(850);
         int expected = 150;
 
-        Assert.assertEquals(actual, expected);
+        assertEquals(actual, expected);
 
     }
 
-    @Test
+    @org.junit.Test
     public void shouldGetCashbackEqualBoundary() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1000);
         int expected = 0;
 
-        Assert.assertEquals(actual, expected);
+        assertEquals(actual, expected);
 
     }
 
-    @Test
+    @org.junit.Test
     public void shouldGetCashbackMoreBoundary() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1001);
         int expected = 999;
 
-        Assert.assertEquals(actual, expected);
+        assertEquals(actual, expected);
 
     }
 }
